207d8ebc8907ebfb2c62511b91868e299080f40d
dengchao <2325690622@qq.com>
2022-02-23 22:50:34 +0800

    refactor: replace layout id with view binding root

--- app/src/main/java/dev/dengchao/clear/my/clipboard/MainActivity.kt
+++ app/src/main/java/dev/dengchao/clear/my/clipboard/MainActivity.kt
@@ -2,10 +2,12 @@ package dev.dengchao.clear.my.clipboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dev.dengchao.clear.my.clipboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
